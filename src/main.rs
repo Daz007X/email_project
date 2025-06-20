34134188b6e6394b56c73c5f239ab0d7c48ae30a
Daz007X <wiwat.pa@kkumail.com>
2025-06-21 00:29:08 +0700

    add env

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use email_project::startup::run;
 use std::net::TcpListener;
 use sqlx::PgPool;
 use env_logger::Env;
+use std::env;
 
 
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
+     println!("RUST_LOG: {:?}", env::var("RUST_LOG"));
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     // สร้าง Port และ Connection string DB 
