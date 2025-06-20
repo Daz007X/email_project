ac5f43b1ab95bd8aa62e585b3160d8b14d20c35a
Wiwat P. <wiwat.pa@kkumail.com>
2025-06-20 21:34:49 +0700

    add tracing span

--- src/handler/subscribe.rs
+++ src/handler/subscribe.rs
@@ -3,10 +3,22 @@ use crate::models::form_data::FormData;
 use sqlx::PgPool;
 use chrono::Utc;
 use uuid::Uuid;
+use tracing::Instrument;
 
 #[post("/subscriptions")]
 pub async fn subscriptions_api(form: web::Form<FormData>,pool :web::Data<PgPool>) -> impl Responder
-{
+{   
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!(
+        "Saving new subscriber details in the database"
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -16,11 +28,17 @@ pub async fn subscriptions_api(form: web::Form<FormData>,pool :web::Data<PgPool>
         form.email,
         form.name,
         Utc::now()
-    ).execute(pool.get_ref()).await
+    )
+    .execute(pool.get_ref())
+    .instrument(query_span)
+    .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => 
+            {
+                HttpResponse::Ok().finish()
+            },
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
